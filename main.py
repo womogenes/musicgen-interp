014d33853c2c99e657469dddae3215198e4a958a
harini-tt <harinitt9@gmail.com>
2025-11-25 23:52:08 -0500

    example code

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
-def main():
-    print("Hello from musigen-interp!")
+from audiocraft.models import MusicGen
+from audiocraft.data.audio import audio_write
 
+model = MusicGen.get_pretrained("small")
+model.set_generation_params(duration=8)  # generate 8 seconds.
 
-if __name__ == "__main__":
-    main()
+descriptions = ["happy rock", "energetic EDM"]
+
+wav = model.generate(descriptions)  # generates 2 samples.
+
+for idx, one_wav in enumerate(wav):
+    # Will save under {idx}.wav, with loudness normalization at -14 db LUFS.
+    audio_write(f'{idx}', one_wav.cpu(), model.sample_rate, strategy="loudness")
