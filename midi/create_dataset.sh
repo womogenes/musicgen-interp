a7da17e9dd6325979a5f31043002054f4f7fcc64
harini-tt <harinitt9@gmail.com>
2025-12-06 17:38:00 -0500

    1000 sample music gen dataset script

--- midi/create_dataset.sh
+++ midi/create_dataset.sh
@@ -2,11 +2,12 @@
 
 #SBATCH --job-name=musicgen_dataset
 #SBATCH -p mit_normal_gpu
-#SBATCH --gres gpu:l40s:1
-#SBATCH --mincpus 4
+#SBATCH --gres gpu:h200:2
+#SBATCH --mincpus 8
 #SBATCH --mem 32000
-#SBATCH --time 12:00:00
+#SBATCH --time 6:00:00
 #SBATCH --output=logs/dataset_creation_%j.out
 #SBATCH --error=logs/dataset_creation_%j.err
 
-uv run make_dataset_with_key.py
+cd /home/harinit9/musicgen-interp/midi
+uv run make_verified_dataset.py
